fb11ce167e16c5f5e0a5b8d93b711542e6f8b0f9
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2024-03-31 20:06:12 -0300

    Add project files.

---
+++ DiamanteLetras.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DiamanteLetras.ConsoleApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("-- Diamante de Letras");
+        Console.WriteLine();
+
+        int n = 5;
+        int px = n;
+        int py = n;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j < n * 2; j++)
+            {
+                if (j == px || j == py)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            px--;
+            py++;
+            Console.WriteLine();
+        }
+    }
+}
