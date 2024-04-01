adc75464a651face108d39afda1ab8123eb7fa43
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2024-04-01 13:35:48 -0300

    quase finalizado

--- DiamanteLetras.ConsoleApp/Program.cs
+++ DiamanteLetras.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace DiamanteLetras.ConsoleApp;
+﻿using System.Runtime.InteropServices;
+
+namespace DiamanteLetras.ConsoleApp;
 
 internal class Program
 {
@@ -7,17 +9,23 @@ internal class Program
         Console.WriteLine("-- Diamante de Letras");
         Console.WriteLine();
 
+        Console.WriteLine("Insira uma letra do alfabeto para formar o diamante: ");
+        char letra = char.Parse(Console.ReadLine());
+
+        string stringAlfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char[] alfabeto = stringAlfabeto.ToCharArray();
+
         int n = 5;
         int px = n;
         int py = n;
 
-        for (int i = 1; i <= n; i++)
+        for (int i = 1; i < n; i++)
         {
             for (int j = 1; j < n * 2; j++)
             {
                 if (j == px || j == py)
                 {
-                    Console.Write("*");
+                    Console.Write(alfabeto[i - 1]);
                 }
                 else
                 {
@@ -26,6 +34,31 @@ internal class Program
             }
             px--;
             py++;
+            Console.WriteLine();
+        }
+
+
+        n = 5;
+        px = 1;
+        py = n * 2 - 1;
+
+        Console.WriteLine(n);
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j < n * 2; j++)
+            {
+                if (j == px || j == py)
+                {
+                    Console.Write(alfabeto[i - 1]);
+
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            px++;
+            py--;
             Console.WriteLine();
         }
     }
